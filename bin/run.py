ec3ce3452d5d97ac578b0e74275b3334402f2fdd
Root-Fang <fangpengdong817@gmail.com>
2014-03-12 10:52:36 +0800

    Add bin direcotory to Git

---
+++ bin/run.py
@@ -0,0 +1,24 @@
+import os
+import sys
+#from twisted.web.wsgi import WSGIResource
+#from twisted.internet import reactor
+#from twisted.web.server import Site
+
+possible_topdir = os.path.normpath(os.path.join(os.path.abspath(
+        sys.argv[0], os.pardir, os.pardir)))
+if os.path.exists(os.path.join(possible_topdir, "allchat", "__init__.py")):
+    sys.path.insert(0, poosible_topdir)
+sys.path.insert(0, possible_topdir)
+
+
+from allchat import app
+from allchat.database import init_db
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug = True, use_debugger = Flase, use_reloader = False)
+#    pool = reactor.getThreadPool()
+#    pool.start()
+#    resource = WSGIResource(reactor, pool, app)
+#    reactor.listenTCP(5000, Site(resource))
+#    reactor.run()
