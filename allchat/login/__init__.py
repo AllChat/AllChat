0cce821f2d7f5276fa38720c4a02c0a426bd5eb5
alen-alex <zzjj0818@gmail.com>
2014-04-10 16:40:38 +0800

    完成了登陆功能

--- allchat/login/__init__.py
+++ allchat/login/__init__.py
@@ -1,1 +1,8 @@
+from flask import Blueprint
+from allchat.login import views
+import allchat
 
+login = Blueprint('login',__name__)
+login_view = views.login_view.as_view('login_view')
+login.add_url_rule('/login/',view_func = login_view, methods = ['GET', ])
+login.add_url_rule('/login/<string:name>',view_func = login_view, methods = ['POST', ])
