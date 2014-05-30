295f6d5b0ae0b99068d20b72b45eb160967e1b39
alen-alex <zzjj0818@gmail.com>
2014-05-30 15:21:08 +0800

    minor adjustment
    获取聊天记录测试例修订

--- test/get_record_test.py
+++ test/get_record_test.py
@@ -1,5 +1,6 @@
 # -*- coding: cp936 -*-
 import time,os,sys
+from pprint import pprint
 
 possible_topdir = os.path.normpath(os.path.join(os.path.abspath(
         sys.argv[0]), os.pardir, os.pardir))
@@ -12,8 +13,8 @@ from allchat.filestore.fileExtract import FileExtractor
 
 if __name__=='__main__':
     extractor = FileExtractor()
-    extractor.getChatRecord('20140204','20140601',
-                            {'type':'user','chatFrom':'alex','chatTo':'derake'})
-    extractor.getChatRecord('20140204','20140601',
-                            {'type':'group','groupName':'allchat','userName':'derake'})
+    pprint(extractor.getChatRecord('20140204','20140601',
+                            {'type':'user','chatFrom':'alex','chatTo':'derake'}))
+    pprint(extractor.getChatRecord('20140204','20140601',
+                            {'type':'group','groupName':'allchat','userName':'derake'}))
     
