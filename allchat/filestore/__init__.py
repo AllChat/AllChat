34be1097129c0cc5686fa639637ffb5e6b61516a
alen-alex <zzjj0818@gmail.com>
2014-06-04 14:10:11 +0800

    在filestore的init里初始化FileSaver对象

--- allchat/filestore/__init__.py
+++ allchat/filestore/__init__.py
@@ -2,3 +2,5 @@ from flask import Blueprint
 from allchat.filestore import fileSave,fileExtract
 
 filestore = Blueprint('filestore', __name__)
+saver = fileSave.FileSaver()
+extractor = fileExtract.FileExtractor()
