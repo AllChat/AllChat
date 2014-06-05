119dd4d9baca79c58f420bf2c1a25d75d5d7c3d9
Root-Fang <fangpengdong817@gmail.com>
2014-06-05 14:34:44 +0800

    布局竖线左侧元素，临时上次

--- static/login.js
+++ static/login.js
@@ -1,3 +1,8 @@
 /**
  * Created by Derek on 2014/6/3.
  */
+$(document).ready(function(){
+    $(".middle-left-image-li").eq(0).hide();
+    $(".middle-left-image-li").eq(1).hide();
+    $(".middle-left-image-li").eq(3).hide();
+});
