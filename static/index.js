0cea139ae8e7d6c50f6ac20ccff52f181fd49ce9
Root-Fang <fangpengdong817@gmail.com>
2014-06-15 20:21:18 +0800

    右侧聊天窗口的字体及图片设置一栏css完成

--- static/index.js
+++ static/index.js
@@ -1,3 +1,10 @@
 /**
  * Created by Derek Fang on 2014/6/11.
  */
+
+$(document).ready(function() {
+    $('#chat-setting form img').on('click', function(e) {
+        e.preventDefault();
+        $(this).next().trigger("click");
+    });
+});
