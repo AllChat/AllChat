b50b3df9d011028e56a1c74e9676576a6458b5d6
Root-Fang <fangpengdong817@gmail.com>
2014-07-25 14:48:35 +0800

    修改了用户登陆状态更新的处理

--- allchat/login/views.py
+++ allchat/login/views.py
@@ -1,9 +1,10 @@
 from flask.views import MethodView
 from flask import request, make_response, g, session
 from allchat.database.sql import get_session
-from allchat.database.models import UserInfo, GroupMember, FriendList
+from allchat.database.models import UserInfo, GroupMember, FriendList, GroupInfo
 from sqlalchemy import and_
 import time, string, base64, threading
+from allchat.amqp.Impl_kombu import send_message
 
 class login_view(MethodView):
     def get(self):
@@ -22,17 +23,23 @@ class login_view(MethodView):
                     def callback(name):
                         db_session = get_session()
                         try:
-                            db_user = db_session.query(UserInfo).filter_by(username = name).one()
+                            db_user = db_session.query(UserInfo).filter(and_(UserInfo.username == name, UserInfo.state != "offline")).one()
                         except Exception, e:
-                            return make_response(("The user is not registered yet", 403, ))
+                            return None
                         db_session.begin()
                         db_user.state = "offline"
                         db_groupmember = db_session.query(GroupMember).filter_by(member_account = name).all()
                         for db_member in db_groupmember:
+                            prev_state = "offline" if db_member.member_logstate != "online" else "online"
                             db_member.member_logstate = "offline"
+                            if prev_state != "offline":
+                                group_update_status(name, db_member.member_account, "offline")
                         db_friendlist = db_session.query(FriendList).filter_by(username = name).all()
                         for db_friend in db_friendlist:
+                            prev_state = "offline" if db_friend.state != "online" else "online"
                             db_friend.state = "offline"
+                            if db_friend.user.state != "offline" and prev_state != "offline":
+                                friendlist_update_status(name, db_friend.user.username, "offline")
                         try:
                             db_session.commit()
                         except:
@@ -63,13 +70,21 @@ class login_view(MethodView):
                 db_user.last_state = db_user.state
                 db_user.state = logstate
                 db_user.login = time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime(time.time()))
-                tmp_state = db_user.state if db_user.state == "online" else "offline"
+                tmp_state = db_user.state
                 db_groupmember = db_session.query(GroupMember).filter_by(member_account = name).all()
                 for db_member in db_groupmember:
+                    prev_state = "offline" if db_member.member_logstate != "online" else "online"
+                    now_state = "offline" if tmp_state != "online" else "online"
                     db_member.member_logstate = tmp_state
+                    if prev_state != now_state:
+                        group_update_status(name, db_member.group_id, now_state)
                 db_friendlist = db_session.query(FriendList).filter_by(username = name).all()
                 for db_friend in db_friendlist:
+                    prev_state = "offline" if db_friend.state != "online" else "online"
+                    now_state = "offline" if tmp_state != "online" else "online"
                     db_friend.state = tmp_state
+                    if db_friend.user.state != "offline" and prev_state != now_state:
+                        friendlist_update_status(name, db_friend.user.username, now_state)
                 try:
                     db_session.commit()
                 except:
@@ -81,7 +96,7 @@ class login_view(MethodView):
                 resp = make_response(("Successful logged in", 200, ))
                 resp.set_cookie("account", value = name)
                 resp.set_cookie("nickname", value = base64.b64encode(db_user.nickname.encode("utf8")))
-                resp.set_cookie("icon", value = str(db_user.icon));
+                resp.set_cookie("icon", value = str(db_user.icon))
                 return resp
             else:
                 return make_response(("Password is wrong, please check out", 403, ))
@@ -89,3 +104,35 @@ class login_view(MethodView):
             return make_response(("Please upload a json data", 403, ))
 
 login_timer = {}
+
+def friendlist_update_status(sender, receiver, state):
+    tmp = {}
+    tmp['method'] = "friendlist_update_status"
+    tmp['from'] = sender
+    tmp['to'] = receiver
+    tmp['time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    tmp['msg'] = state
+    message = {}
+    message['para'] = tmp
+    return send_message(sender, receiver, message)
+
+def group_update_status(sender, group_id, state):
+    db_session = get_session()
+    try:
+        group = db_session.query(GroupInfo).join(GroupMember).filter(and_(GroupInfo.group_id == group_id,
+                GroupMember.member_account != sender, GroupMember.member_logstate != "offline")).one()
+    except:
+        return ("Failed to change group {0}'s state".format(group_id), 403)
+    tmp = {}
+    tmp['method'] = "friendlist_update_status"
+    tmp['from'] = sender
+    tmp['to'] = None
+    tmp['group_id'] = group_id
+    tmp['time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    tmp['msg'] = state
+    message = {}
+    message['para'] = tmp
+    for user in group.groupmembers:
+        message['para']['to'] = user.member_account
+        send_message(sender, user.member_account, message)
+    return None
